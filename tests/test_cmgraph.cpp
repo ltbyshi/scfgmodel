e54dce6db574f0350dc421c12810146f1de445fd
Shi Binbin <ltbyshi@gmail.com>
2013-08-23 20:53:11 +0800

    Corrected some errors.

--- tests/test_cmgraph.cpp
+++ tests/test_cmgraph.cpp
@@ -24,11 +24,14 @@ struct ExpandedNode
 	int type;
 	
 	ExpandedNode(int treeNode, vector<int>& graphNodes,
-		int type = STATE_SS);
-	)
+		int type = STATE_SS)
 		: graphNodes(graphNodes), treeNode(treeNode)
 		{
 		}
+	
+	ExpandedNode()
+	{
+	}
 };
 
 void ExpandNode(ParseTree& tree, 
@@ -45,18 +48,46 @@ void ExpandNode(ParseTree& tree,
 				int ILNode = graph.CreateNode(CMSTATE_IL);
 				int IRNode = graph.CreateNode(CMSTATE_IR);
 				//connect insertion nodes
-				graph.CreateEdge(SNode, IL);
-				graph.CreateEdge(SNode, IR);
+				graph.CreateEdge(SNode, ILNode);
+				graph.CreateEdge(SNode, IRNode);
 				//create insertion cycles
-				graph.CreateEdge(IL, IL);
-				graph.CreateEdge(IR, IR);
+				graph.CreateEdge(ILNode, ILNode);
+				graph.CreateEdge(IRNode, IRNode);
 				//save expandes nodes
 				node.graphNodes.clear();
 				node.graphNodes.push_back(SNode);
 				node.graphNodes.push_back(ILNode);
 				node.graphNodes.push_back(IRNode);
 			}
-			
+			else if(node.type == STATE_SL)
+			{
+				int SNode = graph.CreateNode(CMSTATE_S);
+				graph.CreateEdge(node.graphNodes[0], SNode);
+				//save expandes nodes
+				node.graphNodes.clear();
+				node.graphNodes.push_back(SNode);
+			}
+			else if(node.type == STATE_SR)
+			{
+				int SNode = graph.CreateNode(CMSTATE_S);
+				int ILNode = graph.CreateNode(CMSTATE_IL);
+				graph.CreateEdge(node.graphNodes[0], SNode);
+				graph.CreateEdge(SNode, ILNode);
+				graph.CreateEdge(ILNode, ILNode);
+				//save expanded nodes
+				node.graphNodes.clear();
+				node.graphNodes.push_back(SNode);
+				node.graphNodes.push_back(ILNode);
+			}
+			else
+			{
+			}
+			break;
+		case STATE_L:
+		case STATE_P:
+		case STATE_E:
+		case STATE_E:
+		default:
 	}
 }
 
@@ -66,11 +97,24 @@ void ExpandParseTree(ParseTree& tree, CMGraph& graph)
 	//stack storing tree nodes to be expanded
 	Stack<ExpandedNode> S;
 	S.Push(ExpandedNode(tree.GetRoot(),
-						vector<int>(), STATE_SS));
+						prevNodes, STATE_SS));
 	while(!S.Empty())
 	{
 		ExpandedNode& node = S.Top();
-		
+		ExpandNode(tree, graph, node);
+		if(tree[node.treeNode].state == STATE_B)
+		{
+			//push right branch
+			node.type = STATE_SR;
+			//push left branch
+			ExpandedNode leftNode(node);
+			leftNode.type = STATE_SL;
+			S.Push(leftNode);
+		}
+		else if(tree[node.treeNode].state == STATE_E)
+			S.Pop();
+		else
+			node.treeNode = tree[node.treeNode].lchild;
 	}
 }
 
@@ -78,10 +122,19 @@ void RandomCMGraph(CMGraph& graph, int nNodes)
 {
 	for(int i = 0; i < nNodes; i ++)
 		graph.CreateNode(Choose(ALLCMSTATES, NUMCMSTATES));
+	for(int i = 0; i < nNodes; i ++)
+	{
+		for(int j = 0; j < nNodes / 2; j ++)
+			graph.CreateEdge(i, j);
+	}
 }
 
 void Test_CMGraph()
 {
+	CMGraph graph;
+	RandomCMGraph(graph, 10);
+	PlotCMGraph("graph.dot", graph);
+	system("dot -Tpdf -o graph.pdf graph.dot");
 }
 
 int main()
