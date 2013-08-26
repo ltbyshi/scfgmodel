0ce31fa9d02b224bf309fcd12d1b51d08dba39a0
Shi Binbin <ltbyshi@gmail.com>
2013-08-26 16:59:34 +0800

    Add FromParseTree as member function of CMGraph

--- SConstruct
+++ SConstruct
@@ -3,7 +3,8 @@ env.Append(CCFLAGS = '-g3 -Wall')
 test_cmgraph = env.Program(target = 'bin/test_cmgraph',
 	source = ['src/test_cmgraph.cpp',
 		'src/parsetree.cpp',
-		'src/utils.cpp'])
+		'src/utils.cpp',
+		'src/cmgraph.cpp'])
 test_parsetree = env.Program(target = 'bin/test_parsetree',
 	source = ['src/parsetree.cpp',
 		'src/test_parsetree.cpp',
--- src/cmgraph.h
+++ src/cmgraph.h
@@ -2,7 +2,13 @@
 #define __CMGRAPH_H__
 
 #include <vector>
+#include <algorithm>
 #include "symbol.h"
+#include "parsetree.h"
+
+#define MAX_EXPNODES	6
+#define MAX_MDNODES		4
+#define MAX_INSNODES	2
 
 struct CMGraphNode
 {
@@ -52,12 +58,35 @@ struct CMGraphNode
 	}
 };
 
+//Node to be expanded to from a node in a parse tree
+//Used in to build a CM graph from a parse tree
+struct ExpandedNode
+{
+	//indices of the ancestor nodes in the CM graph
+	int nAncestors;
+	int ancestors[MAX_EXPNODES];
+	//index of the expanded node in the parse tree
+	int treeNode;
+	
+	ExpandedNode(int treeNode, 
+			const int* ancestors,
+			int nAncestors)
+	{
+		this->treeNode = treeNode;
+		this->nAncestors = nAncestors;
+		std::copy(ancestors, ancestors+nAncestors, this->ancestors);
+	}
+	
+	ExpandedNode()
+	{
+	}
+};
+
 class CMGraph
 {
 public:
 	CMGraph()
 	{
-		size = 0;
 	}
 	
 	~CMGraph()
@@ -66,12 +95,20 @@ public:
 	
 	int Size() const
 	{
-		return size;
+		return nodes.size();
 	}
 	
+	//build a CM graph from a parse tree
+	void FromParseTree(ParseTree& tree);
+	//get index of the root node
 	int GetRoot() const
 	{
 		return 0;
+	}
+	//Clear all contents
+	void Clear()
+	{
+		nodes.clear();
 	}
 	//access graph node by index
 	CMGraphNode& operator[](int i)
@@ -87,8 +124,7 @@ public:
 	int CreateNode(CMSTATE state)
 	{
 		nodes.push_back(CMGraphNode(state));
-		size ++;
-		return size - 1;
+		return Size() - 1;
 	}
 	
 	void CreateEdge(int parent, int child)
@@ -129,7 +165,7 @@ public:
 	template <class NodeVisitor>
 	void TraverseNodes(NodeVisitor& visitor)
 	{
-		for(int i = 0; i < size; i ++)
+		for(int i = 0; i < Size(); i ++)
 			visitor(i);
 	}
 	
@@ -138,13 +174,17 @@ public:
 	template <class EdgeVisitor>
 	void TraverseEdges(EdgeVisitor& visitor)
 	{
-		for(int i = 0; i < size; i ++)
+		for(int i = 0; i < Size(); i ++)
 			for(size_t j = 0; j < nodes[i].children.size(); j ++)
 				visitor(i, nodes[i].children[j]);
 	}
 private:
+	//Expand a node of a parse tree into CM states.
+	//The expanded nodes are saved to expNode.ancestors
+	//The tree node to be expanded is pass in expNode.treeNode
+	void ExpandNode(ParseTree& tree, ExpandedNode& expNode);
+private:
 	std::vector<CMGraphNode> nodes;
-	int size;
 };
 
 #endif
