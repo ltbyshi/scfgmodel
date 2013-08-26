481057778067a40acedfa3ee17df75c368bd6adf
Shi Binbin <ltbyshi@gmail.com>
2013-08-26 16:14:56 +0800

    Deleted directory 'src' and rename 'tests' to 'src'

--- src/grammar.h
+++ src/grammar.h
@@ -1,60 +1,52 @@
 #ifndef __GRAMMAR_H__
 #define __GRAMMAR_H__
 
-typedef unsigned int SYMBOL;
+#include <vector>
+#include "matrix.h"
+#include "arraytree.h"
+#include "symbol.h"
 
-struct Production {
+typedef PRECISION double;
+
+class SCFGModel
+{
+public:
+	SCFGModel();
+	~SCFGModel();
+	//Build a model from RNA secondary structure
+	//The structure is in (.) format
+	void Build(const char* structure);
+	
+	PRECISION EmitProb(STATE state, SYMBOL left, SYMBOL right);
+	PRECISION TransProb(STATE curState, STATE nextState);
+	//sample a sequence
+	Vector<SYMBOL> Sample();
+	//optimal sequence (most probable)
+	Vector<SYMBOL> Optimal();
+	//suboptimal sampling
+	Vector<SYMBOL> Suboptimal();
+private:
+	void InitModel(int nStates);
+
+	void Inside(const Vector<SYMBOL>& seq);
+	void Outside(const Vector<SYMBOL& seq);
+	void EMIter();
+private:
 	int nStates;
-	SYMBOL nextStates[2];
-	int emitLeft;
-	int emitRight;
+	ParseTree tree;
+	//pair table
+	std::vector<int> ptable; 
+	std::vector<STATE> states;
+	//e(v, xi, xj): emission probability matrix
+	Matrix3D<PRECISION> em;	
+	//t(v, y): transision probability matrix
+	Matrix2D<PRECISION> tm; 
+	//a(v, i, j): summed probability of all parse subtrees rooted at 
+	//state v for the subsequence [i,...,j]
+	Matrix3D<PRECISION> alpha;
+	//b(v, h, j): summed probability of all parse subtrees rooted at
+	//state v for the complete sequence excluding the subsequence
+	//[i,...,j]
+	Matrix3D<PRECISION> beta;
 };
-
-typedef double PRECISION;
-
-/* terminal symbols */
-#define SYMBOL_A	1
-#define SYMBOL_U	2
-#define SYMBOL_G	3
-#define SYMBOL_C	4
-#define SYMBOL_EPS	5
-
-typedef int STATE;
-/* nonspecific states (nonterminals) */
-#define STATE_ANY	0
-/* specific states */
-#define STATE_P		1
-#define STATE_L		2
-#define STATE_R		3
-#define STATE_D		4
-#define STATE_S		5
-#define STATE_B		6
-#define STATE_E		7
-/* productions */
-const Production PROD_P = {1, {STATE_ANY}, 1, 1};
-const Production PROD_L = {1, {STATE_ANY}, 1, 0};
-const Production PROD_R = {1, {STATE_ANY}, 0, 1};
-const Production PROD_D = {1, {STATE_ANY}, 0, 0};
-const Production PROD_S = {1, {STATE_ANY}, 0, 0};
-const Production PROD_B = {2, {STATE_ANY, STATE_ANY}, 0, 0};
-const Production PROD_E = {0, {}, 0, 0};
-/* SCFG */
-struct SCFGModel {
-	int nStates;
-	STATE* states;
-	PRECISION** emitTable;
-	PRECISION** transTable;
-	int curState;
-};
-
-SCFGModel* CreateSCFGModel(int nStates);
-void FreeSCFGModel(SCFGModel* model);
-PRECISION GetEmitProb(SCFGModel* model, STATE state, 
-		SYMBOL left, SYMBOL right);
-PRECISION GetTransProb(SCFGModel* model, STATE curState,
-		STATE nextState);
-void SampleModel(SCFGModel* model, SYMBOL* seq, int length);
-void Likelihood(SCFGModel* model, SYMBOL* seq, int length);
-void CalcAlpha(SCFGModel* model, SYMBOL*seq, int length);
-void CalcBeta(SCFGModel* model, SYMBOL* seq, int length);
 #endif
--- src/sequence.h
+++ src/sequence.h
@@ -1,13 +1,60 @@
 #ifndef __SEQUENCE_H__
 #define __SEQUENCE_H__
 
+#include <algorithm>
 
-typedef struct Sequence{
-	int length;
-	int* data;
+template <class T>
+class Sequence
+{
+public:
+	Sequence(int capacity = 100)
+	{
+		this->capacity = capacity;
+		size = 0;
+		data = new T[capacity];
+	}
+
+	~Sequence()
+	{
+		if(data)
+			delete data;
+	}
+
+	void Append(const T & e)
+	{
+		if(size >= capacity)
+			Expand();
+		data[size] = e;
+		size ++;
+	}
+
+	int Size() const
+	{
+		return size;
+	}
+
+	T& operator()(int i)
+	{
+		return data[i];
+	}
+
+	T& operator[](int i)
+	{
+		return data[i];
+	}
+private:
+	void Expand()
+	{
+		T* oldData = data;
+		capacity *= 2;
+		data = new T[capacity];
+		std::copy(oldData, oldData + size, data);
+		delete oldData;
+	}
+private:
+	int capacity;
+	int size;
+	T* data;
 };
-
-Sequence* CreateSequence(int length);
-void FreeSequence(Sequence* seq);
 
 #endif
